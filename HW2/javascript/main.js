691e2d8bc8a8f4c58e75c309a2eb9f492a130f25
u21513768 <u21513768@tuks.co.za>
2023-07-31 20:12:27 +0200

    Added files

---
+++ HW2/javascript/main.js
@@ -0,0 +1,64 @@
+let movies = [
+    {
+        id: number = 1, //a unique identifier for this movie
+        title: string = "Reefer Madness", //the title of this movie
+        director: string = "Louis J. Gasnier", //the name of the director of this movie
+        runtime: string = "1h 6m", //the runtime of this movie
+        release_year: number = 1936, //the year this movie was released
+        description: string = "Cautionary tale features a fictionalized take on the use of marijuana" +
+            ". A trio of drug dealers lead innocent teenagers to become addicted to 'reefer' " +
+            "cigarettes by holding wild parties with jazz music.", //a description of this movie
+        poster_url: string = "../images/reeferMadness.jpg", //a URL or file path to an image of the poster for this movie
+        cinema_number: number = 1, //the number of the cinema this movie will be showing in
+        ticket_price: number = 55, //the price of a single ticket for this movie
+        tickets_in_cart: number = 0 //the number of tickets that have been booked for this movie
+    },
+    {
+        id: number = 2, //a unique identifier for this movie
+        title: string = "Robot Monster 3D", //the title of this movie
+        director: string = "Phil Tucker", //the name of the director of this movie
+        runtime: string = "1h 6m", //the runtime of this movie
+        release_year: number = 1953, //the year this movie was released
+        description: string = "The monstrous Ro-Man attempts to annihilate " +
+            "the last family alive on Earth, but finds himself falling for their" +
+            " beautiful daughter.", //a description of this movie
+        poster_url: string = "../images/robotMonster.jpg", //a URL or file path to an image of the poster for this movie
+        cinema_number: number = 2, //the number of the cinema this movie will be showing in
+        ticket_price: number = 75, //the price of a single ticket for this movie
+        tickets_in_cart: number = 0 //the number of tickets that have been booked for this movie
+    },
+    {
+        id: number = 3, //a unique identifier for this movie
+        title: string = "Plan 9 from Outer Space", //the title of this movie
+        director: string = "Edward D. Wood Jr.", //the name of the director of this movie
+        runtime: string = "1h 19m", //the runtime of this movie
+        release_year: number = 1957, //the year this movie was released
+        description: string = "Evil aliens attack Earth and set their terrible " +
+        "'Plan 9' into action. As the aliens resurrect the dead of the Earth, the " +
+        "lives of the living are in danger.", //a description of this movie
+        poster_url: string = "../images/plan9FromOuterSpace.jpg", //a URL or file path to an image of the poster for this movie
+        cinema_number: number = 3, //the number of the cinema this movie will be showing in
+        ticket_price: number = 55, //the price of a single ticket for this movie
+        tickets_in_cart: number = 0 //the number of tickets that have been booked for this movie
+    },
+    {
+        id: number = 4, //a unique identifier for this movie
+        title: string = "Eegah", //the title of this movie
+        director: string = "Arch Hall Sr.", //the name of the director of this movie
+        runtime: string = "1h 32m", //the runtime of this movie
+        release_year: number = 1962, //the year this movie was released
+        description: string = "While driving through the desert, a teenage girl" +
+        " is frightened by a seven-foot giant which appears in her path. After" +
+        " escaping, she returns to the site with her boyfriend and her father in" +
+        " an attempt to find the giant. They do, and it proceeds to terrorize them" +
+        " and the rest of Palm Springs, California.", //a description of this movie
+        poster_url: string = "../images/eegah.jpg", //a URL or file path to an image of the poster for this movie
+        cinema_number: number = 4, //the number of the cinema this movie will be showing in
+        ticket_price: number = 45, //the price of a single ticket for this movie
+        tickets_in_cart: number = 0 //the number of tickets that have been booked for this movie
+    }
+];
+    
+function populate(){
+    
+}
