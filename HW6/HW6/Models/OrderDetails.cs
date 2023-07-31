0dcf00f2615891113340f47e2130b7265ca52c57
u21513768 <u21513768@tuks.co.za>
2023-07-31 20:15:18 +0200

    Added files

---
+++ HW6/HW6/Models/OrderDetails.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+//<!--Quintin d' Hotman de Villiers u21513768-->
+namespace HW6.Models
+{
+    public class OrderDetails
+    {
+        public order Order { get; set; }
+        public order_items OrderItem { get; set; }
+        public product Product { get; set; }
+    }
+}
---
+++ HW6/HW6/Models/Report.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+//<!--Quintin d' Hotman de Villiers u21513768-->
+namespace HW6.Models
+{
+    public class Report
+    {
+        //public List<order> Order { get; set; }
+        //public IQueryable<int> Years { get; set; }
+        //public string[] Months { get; set; }
+        public List<int> Totals { get; set; }
+    }
+}
